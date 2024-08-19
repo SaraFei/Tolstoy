b8e2386265a51f9368bb00f94cffb2f723627987
SaraFei <s8581333@gmail.com>
2024-08-19 19:21:39 +0300

    end

--- server/index.js
+++ server/index.js
@@ -10,13 +10,8 @@ import dataWebRouter from "./routs/DataWeb.js"
 config();
 const app=express();
 app.use(express.json());
-// app.use(cors({ origin: "*" }));//During development allows all sources
-app.use(cors({
-    origin: 'https://client-alpha-two.vercel.app', // כתובת ה-URL של ה-client שלך
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type']
-  }));
-  
+app.use(cors({ origin: "*" }));//During development allows all sources
+
 const limiter = rateLimit({
     windowMs: 1000, // 1 second window
     max: 5, // Limit each IP to 5 requests per windowMs
